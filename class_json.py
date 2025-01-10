e024550e44239e262405f444641d42741c91c626
BrianHammond <brianearlhammond@gmail.com>
2025-01-11 08:33:43 +0900

    Initial Commit

---
+++ class_json.py
@@ -0,0 +1,43 @@
+# Python file detection
+# Python writing files (.txt, .json, .csv)
+
+# writing modes = 'w' will write the txt_data and create a new file, or overwrite the txt_data if file already available  )
+#                 'x' will fail to write if a file already exists
+#                 'a' will append new text data
+#
+
+
+import json
+
+class Initialize_JSON:
+    def initialize_json(self):
+        json_string = '{"employees":[]}'
+
+        data = json.loads(json_string)
+
+        with open("files/data.json", "w") as file: # this will write the txt_data and create a new file, or overwrite the txt_data if file already available 
+            json.dump(data, file, indent=4) # json.dump converts the dictionary into a json string, 'file' as the second argument and indent to create indentations
+            print(f"json file was created")
+
+class JSON:
+
+    def __init__(self):
+
+        self.name = input("Enter employee name: ")
+        self.age = int(input("Enter employee age: "))
+        self.title = input("Enter employee title: ")
+        self.misc = input("Enter miscellaneous information: ")
+
+        self.employee = {
+                            "name": self.name, 
+                            "age": self.age, 
+                            "title": self.title, 
+                            "misc": [self.misc]
+                        }
+
+    def appending(self):
+        with open("files/data.json", "r+") as file:
+            file_content = json.load(file)
+            file_content["employees"].append(self.employee)
+            file.seek(0)
+            json.dump(file_content, file, indent=4)
---
+++ main.py
@@ -0,0 +1,44 @@
+# Python file detection
+# Python writing files (.txt, .json, .csv)
+
+# writing modes = 'w' will write the txt_data and create a new file, or overwrite the txt_data if file already available  )
+#                 'x' will fail to write if a file already exists
+#                 'a' will append new text data
+#
+
+import os
+from class_json import *
+
+menu = ("""
+1. Initialize JSON
+2. Writing JSON     
+3. Reading JSON
+0. Exit  
+""")
+
+while True:
+    print(menu)
+    choice = int(input("Enter Choice: "))
+
+    match choice:
+        case 0:
+            break
+        case 1:
+            if os.path.exists("files/data.json") == False: # checks to see if the file is there (returns a bool)
+                print(f"data.json not found, creating a new one")
+                Initialize_JSON().initialize_json()
+            elif os.path.exists("files/data.json") == True:
+                print(f"JSON already initialized")
+        case 2:
+            try:
+                if os.path.exists("files/data.json") == False: # checks to see if the file is there (returns a bool)
+                    print(f"data.json not found, please Initialize")
+                elif os.path.exists("files/data.json") == True:
+                    JSON().appending()
+            except ValueError:
+                print("Please check the values you entered, age can only accept numbers")
+        case 3:
+            print("I haven't gotten to this point yet")
+        case _:
+            print("enter a valid number")
+print("GOOD BYE")
